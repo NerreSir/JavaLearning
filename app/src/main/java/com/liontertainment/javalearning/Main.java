a1fd41e659b55b74b525b37bc575866cb6b1b475
nerresir <bugraarslan96@gmail.com>
2022-02-22 17:41:18 +0300

    JavaLearning-B18

---
+++ app/src/main/java/com/liontertainment/javalearning/Main.java
@@ -0,0 +1,29 @@
+package com.liontertainment.javalearning;
+
+public class Main {
+
+    public static void main(String[] args){
+
+
+
+
+/*         ---BÖLüm18 Variables---
+        int age = 20;
+        long height = 185;
+        System.out.println(height);
+        System.out.println(age);
+        System.out.println(age/5);
+
+        double x = 5.0;
+        double y = 11.3;
+        float z = 14.6f;
+        System.out.println(y/x);
+        System.out.println(z/y);
+*/
+
+/*         ---BÖLÜM17---
+        System.out.println("hello java");
+        System.out.println(5 * 2);
+*/
+    }
+}
