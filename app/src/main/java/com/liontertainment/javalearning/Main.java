db8e8e482ac249704ac04eda1b4b9360d67c6b61
nerresir <bugraarslan96@gmail.com>
2022-02-22 17:59:49 +0300

    JavaLearning-B20

--- app/src/main/java/com/liontertainment/javalearning/Main.java
+++ app/src/main/java/com/liontertainment/javalearning/Main.java
@@ -5,9 +5,34 @@ public class Main {
     public static void main(String[] args){
 
 
+/*
+        //---Final--- Değerin başına Final yazarak sabit hale getiririz
+        final int myinteger = 5;
+        System.out.println("myinteger: "+ myinteger);
 
+        myinteger =4;
+        System.out.println("myinteger: "+ myinteger);
+*/
+/*      ---Bölüm19 Variables Devamı---
+        double pi = 3.14;
+        int r = 5;
+        System.out.println(pi*r*2);
 
-/*         ---BÖLüm18 Variables---
+        //String
+        String name = "James";
+        String surname = "Hetfield";
+        String fullname = name +" "+ surname;
+        System.out.println(name);
+        System.out.println(surname);
+        System.out.println(fullname);
+
+        //Boolean Doğru-Yanlış
+        boolean İsAlive = true;
+        İsAlive = false;
+        System.out.println(İsAlive);
+*/
+/*
+        ---BÖLüm18 Variables---
         int age = 20;
         long height = 185;
         System.out.println(height);
@@ -20,8 +45,8 @@ public class Main {
         System.out.println(y/x);
         System.out.println(z/y);
 */
-
-/*         ---BÖLÜM17---
+/*
+        ---BÖLÜM17---
         System.out.println("hello java");
         System.out.println(5 * 2);
 */
