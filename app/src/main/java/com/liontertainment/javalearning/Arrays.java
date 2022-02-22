52b8368fd031f94e3a367cbdaa22c22943855c97
nerresir <bugraarslan96@gmail.com>
2022-02-22 18:16:33 +0300

    JavaLearning-B21

---
+++ app/src/main/java/com/liontertainment/javalearning/Arrays.java
@@ -0,0 +1,29 @@
+package com.liontertainment.javalearning;
+
+public class Arrays {
+    public static void main(String[] args){
+
+
+
+/*      ---B21---
+        String[] myStringArray = new String[4];
+        myStringArray[0] = "James";
+        myStringArray[1] = "Lars";
+        myStringArray[2] = "Kirk";
+        myStringArray[3] = "Bob";
+
+        System.out.println(myStringArray[1]);
+
+        int[] myIntegerArray = new int[4];
+        myIntegerArray[0] = 50;
+        myIntegerArray[1] = 60;
+        myIntegerArray[2] = 70;
+        myIntegerArray[3] = 80;
+
+        System.out.println(myIntegerArray[1]);
+
+        int[] myNumberArray = {1,2,3,4,5,6,7,8};
+        System.out.println(myNumberArray[2]);
+*/
+    }
+}
