4bb07f47f06f3cd8c5d9ef443fcb0e2c5607a322
nerresir <bugraarslan96@gmail.com>
2022-02-24 19:03:20 +0300

    JavaLearning-B28.2

---
+++ app/src/main/java/com/liontertainment/javalearning/Loops.java
@@ -0,0 +1,22 @@
+package com.liontertainment.javalearning;
+
+public class Loops {
+    public static void main(String[] args) {
+
+        //for Loop
+
+        int [] myNumbers = {12,15,18,21,24};
+
+        //System.out.println(x);
+
+        for (int i = 0; i<myNumbers.length; i++){
+            int x = myNumbers[i] / 3 * 5;
+            System.out.println(x);
+        }
+
+
+
+
+
+    }
+}
