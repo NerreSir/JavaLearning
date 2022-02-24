e3156a81ff9a0d4180a8aa751b2ffb515e4aebd1
nerresir <bugraarslan96@gmail.com>
2022-02-24 18:29:45 +0300

    JavaLearning-B28

--- app/src/main/java/com/liontertainment/javalearning/Arrays.java
+++ app/src/main/java/com/liontertainment/javalearning/Arrays.java
@@ -1,11 +1,53 @@
 package com.liontertainment.javalearning;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Arrays {
     public static void main(String[] args){
+/*
+        //B24 Hashmap Dizilerdeki indeksler yerine tanım yapmaya yarar
+        HashMap<String, String> myhashmap = new HashMap<String, String>();
+        myhashmap.put("name","james");
+        myhashmap.put("instrument", "gitar");
+
+        System.out.println(myhashmap.get("name"));
+        System.out.println(myhashmap.get("instrument"));
 
 
+        HashMap<String, Integer> mysecondmap = new HashMap<>();
+        mysecondmap.put("run", 100);
+        mysecondmap.put("basketball",200);
+        System.out.println(mysecondmap.get("run"));
+*/
 
-/*      ---B21---
+/*
+        //---B23 Set---
+        HashSet<String> myset = new HashSet<>();
+        myset.add("james");
+        myset.add("james");
+
+        System.out.println(myset.size());
+*/
+
+/*
+        //---B22 Lists (Dizilerden çok daha rahat sınırlama getirmek zorunda kalmıyosun
+        ArrayList<String> mymusicians = new ArrayList<>();
+        mymusicians.add("James");
+        mymusicians.add("lars");
+        mymusicians.add(1,"Kirk"); //Kirk ü 1.indexe sabitledi
+        mymusicians.add("Rob");
+
+        System.out.println(mymusicians.get(0));
+        System.out.println(mymusicians.get(1));
+        System.out.println(mymusicians.get(2));
+        System.out.println(mymusicians.get(3));
+        System.out.println(mymusicians.size());
+*/
+
+/*
+        //---B21 Array (Diziler)---
         String[] myStringArray = new String[4];
         myStringArray[0] = "James";
         myStringArray[1] = "Lars";
