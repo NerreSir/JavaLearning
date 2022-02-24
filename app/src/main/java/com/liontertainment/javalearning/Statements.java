43f3078e4f1be521117fff44557e0686ac80a0a4
nerresir <bugraarslan96@gmail.com>
2022-02-24 18:29:58 +0300

    JavaLearning-B28

---
+++ app/src/main/java/com/liontertainment/javalearning/Statements.java
@@ -0,0 +1,103 @@
+package com.liontertainment.javalearning;
+
+public class Statements {
+
+    public static void main (String[] args){
+/*
+        //B27 Switch
+        int day = 3;
+        String dayString = "";
+
+        if (day == 1) {
+            dayString = "---Mon---";
+        } else if (day == 2) {
+            dayString = "---Tue---";
+        } else if (day == 3) {
+            dayString = "---Wed---";
+        } else {
+            dayString = "---Error---";
+        }
+
+        System.out.println(dayString);
+
+
+
+        switch (day){
+
+            case 1:
+                dayString = "mon";
+                break;
+            case 2:
+                dayString = "tue";
+                break;
+            case 3:
+                dayString = "wed";
+                break;
+            default:
+                dayString = "error";
+                break;
+        }
+        System.out.println(dayString);
+*/
+
+
+
+/*
+        //B26 If Statements
+        int x = 3;
+        int y = 4;
+
+        if (x<y)
+        { System.out.println("y daha büyük"); }
+        else if (y<x)
+        { System.out.println("x daha büyük"); }
+        else
+        { System.out.println("x y ile eşittir"); }
+*/
+
+
+
+/*
+        //B25 Operatörler Statements
+
+        int x = 5;
+        System.out.println(x);
+        x = x+1;
+        System.out.println(x);
+        x++;
+        System.out.println(x);
+        x--;
+        System.out.println(x);
+        x = x * 5;
+        System.out.println(x);
+
+        int y = 4;
+        System.out.println(x>y); //sonucu boolean verir T/F
+        System.out.println(x<y);
+
+        y = 30;
+        System.out.println(x>=y);
+        System.out.println(x==y);
+        System.out.println(x!=y);
+
+        //and&&
+        //or ||
+
+        x=3;
+        y=4;
+        int z=5;
+
+        //AND
+        System.out.println("---AND---");
+        System.out.println(x<y && y<z); //ikiside doğru iken True
+        System.out.println(x<y && y>z); //ikisinden biri yanlış ise False
+        System.out.println(x>y && y>z); //ikiside yanlışsa False
+
+        //OR
+        System.out.println("---OR---");
+        System.out.println(x<y || y<z); //ikiside doğru iken True
+        System.out.println(x<y || y>z); //ikisinden biri yanlış ise True
+        System.out.println(x>y || y>z); //ikiside yanlışsa False
+*/
+    }
+}
