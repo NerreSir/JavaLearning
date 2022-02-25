058f64b4720f0f309dc224e4f41671b870d8629e
nerresir <bugraarslan96@gmail.com>
2022-02-25 16:14:34 +0300

    JavaLearning-B30

--- app/src/main/java/com/liontertainment/javalearning/Loops.java
+++ app/src/main/java/com/liontertainment/javalearning/Loops.java
@@ -7,16 +7,27 @@ public class Loops {
 
         int [] myNumbers = {12,15,18,21,24};
 
-        //System.out.println(x);
-
         for (int i = 0; i<myNumbers.length; i++){
             int x = myNumbers[i] / 3 * 5;
             System.out.println(x);
         }
 
+        for (int number : myNumbers){
+            System.out.println(number / 3 * 5);
+        }
 
+        for (int a=0; a < 10; a++){
+            int b = a * 10;
+            System.out.println(b);
+        }
 
+        //B30 While
 
+        int j = 0;
+        while (j <  3){
+            System.out.println("test");
+            j = j + 1;
+        }
 
     }
 }
